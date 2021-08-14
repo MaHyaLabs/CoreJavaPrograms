e259d840f9957d1241361a6aa82787a632a2395e
MaHyatakkalwad <MaHyatakkalwad@users.noreply.github.com>
2021-08-14 21:07:07 +0530

    Update WelcomeToJava.java

--- src/test/java/WelcomeToJava.java
+++ src/test/java/WelcomeToJava.java
@@ -2,5 +2,6 @@ public class WelcomeToJava {
 
     public static void main(String[] args){
         System.out.println("Welcome To CoreJavaPrograms Session");
+        System.out.println("This is the First Commit");
     }
 }
